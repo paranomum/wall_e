3934c1ab050cc27c26b9aff5388df1d7dfdd747d
paranomum <easokolova.sherbert@gmail.com>
2024-07-18 14:36:10 +0300

    добавила cli для генерации iqhr

---
+++ modules/openapi-generator-cli-iqhr/src/main/java/org/openapitools/codegen/cmd/OpenApiGeneratorCommand.java
@@ -0,0 +1,43 @@
+package org.openapitools.codegen.cmd;
+
+import io.airlift.airline.Help;
+import io.airlift.airline.model.GlobalMetadata;
+
+import javax.inject.Inject;
+
+@SuppressWarnings({"java:S106"})
+public abstract class OpenApiGeneratorCommand implements Runnable {
+
+    public final String DEFAULT_GENERATOR_NAME = "java";
+//    public final String DEFAULT_API_PACKAGE = "";
+
+    @Inject
+    public GlobalOptions globalOptions = new GlobalOptions();
+
+    @Inject
+    public GlobalMetadata global;
+
+    protected BuildInfo buildInfo = new BuildInfo();
+
+    @Override
+    public void run() {
+        if (globalOptions.version) {
+            System.out.println(buildInfo.versionDisplayText());
+            return;
+        }
+
+        if (globalOptions.help) {
+            Help help = new Help();
+            help.global = global;
+            help.call();
+            return;
+        }
+
+        execute();
+    }
+
+    /**
+     * Logic to be executed by implementing commands
+     */
+    abstract void execute();
+}
